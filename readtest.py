4099a4ecfef5c09e19214b1393d8c0ec4024b4dd
Björn <bjorn@ekelund.nu>
2021-12-07 18:42:59 +0100

    Update

--- readtest.py
+++ readtest.py
@@ -4,25 +4,28 @@ import socket, _thread
 from termcolor import colored
 from time import sleep
 from spot import Spot
+from multiprocessing import Process
 from entities import Entities
+from threading import Thread
+import threading 
+import re
+
  
 MYCALL = 'SM7IUN-8'
 #host, port = 'clx.noantri.org', 23
-host, port = 'dxc.w9pa.net', 7373
-server = (host, port)
+# host1, port1 = 'dxc.w9pa.net', 7373
+# server = (host, port)
  
-clxsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-entities = Entities()
- 
-def sendCmd(txt):
+def sendCmd(socket, txt):
     ''' Send a string to server. Carry self included'''
     txt = txt + '\n'
-    clxsock.send(txt.encode())
+    socket.send(txt.encode())
      
-def connection():
+def connection(host, port, clxsock):
     ''' Make connetion to server'''
-    global clxsock
+    # global clxsock
     connected = False
+    server = (host, port)
     while not connected:
         print(colored(f'Connection to: {host} {port}', 'blue'))
         try:
@@ -34,32 +37,168 @@ def connection():
             print('Connected!!!')
             connected = True
          
-def run():
-    connection()
+def run1(host, port, node, clxsock):
+    connection(host, port, clxsock)
     while True:
         try:
             clxmsg = clxsock.recv(2048).decode('latin-1')
             for line in clxmsg.splitlines():
                 if line.upper().startswith('DX DE '):
                     #SPOT
-                    spot = Spot(line)
+                    spot = Spot(line, node)
                     print(spot.toString())
                 else:
                     print(colored(line, 'yellow'))
                     if 'login' in line.lower() or 'enter your call' in line.lower():
-                        sendCmd(MYCALL)
+                        print('Sending call')
+                        sendCmd(clxsock, MYCALL + '\n')
                         # sendCmd("set/name Bjorn\n")
                         # sendCmd("set/QTH Bjarred\n")
                         # sendCmd("set/QRA JO65mr\n")
                         # sendCmd("set/nobell\n")
-                        sendCmd("set dx extension skimmerquality\n")
-                         
+                        # sendCmd(clxsock, "set dx extension skimmerquality\n")                         
         except KeyboardInterrupt:
-            sendCmd("bye\n")
+            sendCmd(clxsock, "BYE\n")
             print('Disconnected')
-             
+            clxsock.close()
+            exit()
+    
+class w9pa(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        clxsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connected = False
+        host = 'dxc.w9pa.net'
+        port = 7373
+        server = (host, port)
+        while not connected:
+            print(colored(f'Connection to: {host} {port}', 'blue'))
+            try:
+                clxsock.connect(server)
+            except:
+                print('Impossibile to connect')
+                sleep(5)     
+            else:       
+                print('Connected!!!')
+                connected = True
+
+        while True:
+            try:
+                clxmsg = clxsock.recv(2048).decode('latin-1')
+                for line in clxmsg.splitlines():
+                    if line.upper().startswith('DX DE '):
+                        #SPOT
+                        spot = Spot(line, 'W9PA')
+                        print(spot.toString())
+                    else:
+                        print(colored(line, 'yellow'))
+                        if 'login' in line.lower() or 'enter your call' in line.lower():
+                            print('Sending call')
+                            sendCmd(clxsock, MYCALL + '\n')
+                            # sendCmd("set/name Bjorn\n")
+                            # sendCmd("set/QTH Bjarred\n")
+                            # sendCmd("set/QRA JO65mr\n")
+                            # sendCmd("set/nobell\n")
+                            # sendCmd(clxsock, "set dx extension skimmerquality\n")                         
+            except KeyboardInterrupt:
+                sendCmd(clxsock, "BYE\n")
+                print('Disconnected')
+                clxsock.close()
+                exit()
+    
+class rbn(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        
+    def run(self):
+        clxsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connected = False
+        host = 'telnet.reversebeacon.net'
+        port = 7000
+        server = (host, port)
+        while not connected:
+            print(colored(f'Connection to: {host} {port}', 'blue'))
+            try:
+                clxsock.connect(server)
+            except:
+                print('Impossibile to connect')
+                sleep(5)     
+            else:       
+                print('Connected!!!')
+                connected = True
+
+        while True:
+            try:
+                clxmsg = clxsock.recv(2048).decode('latin-1')
+                for line in clxmsg.splitlines():
+                    if line.upper().startswith('DX DE '):
+                        #SPOT
+                        spot = Spot(line, 'EBN')
+                        print(spot.toString())
+                    else:
+                        print(colored(line, 'yellow'))
+                        if 'login' in line.lower() or 'enter your call' in line.lower():
+                            print('Sending call')
+                            sendCmd(clxsock, MYCALL + '\n')
+            except KeyboardInterrupt:
+                sendCmd(clxsock, "BYE\n")
+                print('Disconnected')
+                clxsock.close()
+                exit()
+    
 if __name__ == '__main__':
-    _thread.start_new_thread(run, ())
-    while True:
-        command = input('>')
-        sendCmd(command)
+    # clxsock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # clxsock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # threads = []
+    # Thread(target = run1('dxc.w9pa.net', 7373, 'W9PA', clxsock1)).start()
+    # Thread(target = run2('telnet.reversebeacon.net', 7000, 'RBN', clxsock2)).start()
+    
+    # thread1 = w9pa()
+    # thread1.start()
+    # thread1.join()
+
+    thread2 = rbn()
+    thread2.start()
+    thread2.join()
+
+    # threads = []
+    # print('Starting W9PA')
+    # t = threading.Thread(target=w9pa(), args=(0,))
+    # t.start()
+    # threads.append(t)
+
+    # print('Starting RBN')
+    # t = threading.Thread(target=run2('telnet.reversebeacon.net', 7000, 'RBN', clxsock2), args=(1,))
+    # t.start()
+    # threads.append(t)
+
+    # p1 = Process(target=run1('dxc.w9pa.net', 7373, 'W9PA', clxsock1), args=(0,))
+    # # Pros.append(p1)
+    
+    # p2 = Process(target=run2('telnet.reversebeacon.net', 7000, 'RBN', clxsock2), args=(1,))
+    # Pros.append(p2)
+
+    # print('Starting W9PA')
+    # # p1.start()
+    # print('Starting RBN')
+    # p2.start()
+    
+    # for t in threads:
+        # t.join()
+    
+    # for t in Pros:
+        # t.join()
+
+    # _thread.start_new_thread(run('dxc.w9pa.net', 7373, 'W9PA', clxsock1), ())
+    # _thread.start_new_thread(run('telnet.reversebeacon.net', 7000, 'RBN', clxsock2), ())
+    # print('Done!')
+    # clxsock1.close()
+    # clxsock2.close()
+    # while True:
+        # command = input('>')
+        # sendCmd(command)
--- spot.py
+++ spot.py
@@ -3,29 +3,42 @@ import re
  
 class Spot(): 
      
-    def __init__(self, line):
+    def __init__(self, line, node):
         self.txt = line
         colon, period = line.find(":"), line.find(".")
         self.spotter = line[6 : colon]
-        self.qrg = line[colon + 1 : period + 2].strip()
-        self.callsign = line[period + 3 : 39].strip()
+        # self.qrg = line[colon + 1 : period + 2].strip()
+        self.qrg = re.findall("\d+\.\d+", line.partition(":")[2])[0]
+        # print(node + ':' + 'Line=" + line)
+        print(node + ': part=' + line.partition(":")[2])
+        
+        self.callsign = re.findall("[A-Z0-9\/]+[A-Z][A-Z0-9\/]+", line.partition(":")[2])[0]
+        # print(node + ': call=' + self.callsign)
+        # for mat in re.finditer(" [A-Z0-9\/]+ ", line.partition(":")[2]):
+            # if (mat <> None) 
+                # print("2nd half =" + line.partition(":")[2])
+                # s = mat.start()
+                # e = mat.end()
+                # self.callsign = line[s + 1 : e - 1]
+        # self.callsign = line[period + 3 : 39].strip()
+        # Time
         for mat in re.finditer(" \d\d\d\d[zZ]", line):
-            s = mat.start()
-            e = mat.end()
-        self.time = line[s + 1 : e - 1]
+            s2 = mat.start()
+            e2 = mat.end()
+            self.time = line[s2 + 1 : e2 - 1]
 
-        tag = line[s - 2 : s + 1]
-        self.corrected = ''
+        tag = line[s2 - 2 : s2 + 1]
+        # print(node + ': tag=' + tag)
+        self.corrected = self.callsign
         if (re.match("^ [V?QB] $", tag)):
             self.quality = tag[1 : 2]
             if ((self.quality == '?' or self.quality == 'B') and re.match(".+\([A-Z0-9]+\)", line)):
-                for mat2 in re.finditer("\([A-Z0-9]+\)", line):
-                    s2 = mat2.start()
-                    e2 = mat2.end()
-                self.corrected = line[s2 + 1 : e2 - 1]
+                print('Correcting call')
+                for mat in re.finditer("\([A-Z0-9]+\)", line):
+                    s3 = mat.start()
+                    e3 = mat.end()
+                self.corrected = line[s3 + 1 : e3 - 1]
                 self.quality = 'C'
-            else:
-                self.corrected = self.callsign
         else:
             self.quality = 'N'
          
